d204b6aa8a475c534add6a794b706f08b9010fa5
Artur MiRgaleev <easy0@inbox.ru>
2021-04-03 18:22:20 +0500

    added manager pakage

---
+++ src/main/java/post/manager/PostManager.java
@@ -0,0 +1,13 @@
+package post.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+
+    public Post[] search(int ownerID, String domain, String query, boolean ownersOnly, byte count, int offset) {
+        return null;
+    }
+
+    public void delete(int ownerID, int postID) {
+    }
+}
